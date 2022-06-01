86e648e7aca904e085665409ed119a8da54c75db
Primož Žnidar <sg.prima@gmail.com>
2022-06-02 00:57:04 +0200

    System black color

--- app/src/main/java/com/example/matrixdownfall/ui/screen/MatrixScreen.kt
+++ app/src/main/java/com/example/matrixdownfall/ui/screen/MatrixScreen.kt
@@ -2,6 +2,7 @@ package com.example.matrixdownfall.ui.screen
 
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -10,16 +11,32 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.TextUnit
 import com.example.matrixdownfall.util.characters
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.delay
 import kotlin.random.Random
 
 @Composable
 fun MatrixScreen(){
+    // Remember a SystemUiController
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+
+    SideEffect {
+        // Update all of the system bar colors to be transparent, and use
+        // dark icons if we're in light theme
+        systemUiController.setSystemBarsColor(
+            color = Color.Black,
+            darkIcons = useDarkIcons
+        )
+
+        // setStatusBarsColor() and setNavigationBarColor() also exist
+    }
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.Black
     ) {
-        MatrixRain(30)
+        MatrixRain(20)
     }
 }
 
@@ -55,7 +72,7 @@ fun RowScope.MatrixColumn(crawlSpeed: Long, columnDelay: Long, oneTime: Boolean,
     BoxWithConstraints(
         modifier = Modifier.weight(1f).fillMaxHeight()
     ) {
-        val maxWidthDp = maxWidth
+        val fontSize = with(LocalDensity.current){ maxWidth.toSp() }
         val matrixStrip = remember { Array((maxHeight / maxWidth).toInt()){ characters.random() } }
         var lettersToDraw by remember { mutableStateOf(0) }
 
@@ -64,7 +81,7 @@ fun RowScope.MatrixColumn(crawlSpeed: Long, columnDelay: Long, oneTime: Boolean,
                 MatrixChar(
                     matrixStrip[it],
                     crawlSpeed,
-                    fontSize = with(LocalDensity.current){ maxWidthDp.toSp() },
+                    fontSize = fontSize,
                     onFinished = {
                         if(oneTime && it == (matrixStrip.size * 0.6).toInt()){
                             onFinished()
@@ -85,12 +102,11 @@ fun RowScope.MatrixColumn(crawlSpeed: Long, columnDelay: Long, oneTime: Boolean,
                     lettersToDraw += 1
                 }
 
-                if(lettersToDraw > matrixStrip.size * 0.5){
+                if(lettersToDraw > matrixStrip.size * 0.6){
                     matrixStrip[Random.nextInt(lettersToDraw)] = characters.random()
                 }
 
                 delay(crawlSpeed)
-
             }
         }
     }
